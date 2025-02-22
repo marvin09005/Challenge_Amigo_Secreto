11612ec461f68f6d20165287714e180fb257542a
marvin09005 <marvingt2500@gmail.com>
2025-02-22 14:25:30 -0600

    Crea arreglo y funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arregloaAmigos = [];
+
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (nombreAmigo === "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    arregloaAmigos.push(nombreAmigo);
+    document.getElementById("amigo").value = '';
+  }
+}
