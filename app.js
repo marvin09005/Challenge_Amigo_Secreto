375bbb9690760accc77fa7f6175b0b18aa5153fe
marvin09005 <marvingt2500@gmail.com>
2025-02-22 14:53:33 -0600

    Crea funcion sortearAmigo

--- app.js
+++ app.js
@@ -9,17 +9,33 @@ function agregarAmigo() {
     alert("Por favor, inserte un nombre.");
   } else {
     arregloaAmigos.push(nombreAmigo);
-    document.getElementById("amigo").value = '';
-    actualizarLista()
+    document.getElementById("amigo").value = "";
+    actualizarLista();
   }
 }
 
 function actualizarLista() {
-    let lista = document.getElementById("listaAmigos");
+  let lista = document.getElementById("listaAmigos");
 
-    lista.innerHTML = '';
+  lista.innerHTML = "";
 
-    for (let numeroAmigos = 0; numeroAmigos < arregloaAmigos.length; numeroAmigos++) {
-        lista.innerHTML += `<li>${arregloaAmigos[numeroAmigos]}</li>`
-    }
+  for (
+    let numeroAmigos = 0;
+    numeroAmigos < arregloaAmigos.length;
+    numeroAmigos++
+  ) {
+    lista.innerHTML += `<li>${arregloaAmigos[numeroAmigos]}</li>`;
+  }
+}
+
+function sortearAmigo() {
+  if (arregloaAmigos.length === 0) {
+    alert("No hay amigos para sortear.");
+  } else {
+    let amigoAleatorio = Math.floor(Math.random() * arregloaAmigos.length);
+    let amigoSorteado = arregloaAmigos[amigoAleatorio];
+    let amigoSecreto = document.getElementById('resultado');
+
+    amigoSecreto.innerHTML = `<li>${amigoSorteado}</li>`
+  }
 }
